545846574cce71fd852da37c3ec455331f1ad003
Anh Dinh <anhdinh17@gmail.com>
2024-08-04 20:49:08 +0700

    Initial Commit

---
+++ MessengerTutorial/MessengerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTutorialApp.swift
+//  MessengerTutorial
+//
+//  Created by Anh Dinh on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
